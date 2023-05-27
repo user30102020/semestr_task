c745e020549d0fe8a3fff40f70f13b82c76ccc53
username30102020 <bril.maks19.04@mail.ru>
2023-05-27 15:02:38 +0300

    corrected

--- first_year/task_2/src/model/ShellSort.java
+++ first_year/task_2/src/model/ShellSort.java
@@ -10,20 +10,11 @@ public class ShellSort<T extends Comparable<T>> {
 
     public void sort() {
         int iterations_counter = 0;
-        int x = 0;
-        int y = 0;
-        int z = 0;
         for (int step = arr.length / 2; step > 0; step /= 2) {
-            x++;
-            iterations_counter += y;
-            y = 0;
             for (int i = step; i < arr.length; i++) {
-                y++;
-                iterations_counter += z;
-                z = 0;
                 int j = i;
                 while (j >= step && arr[j].compareTo(arr[j - step]) < 0){
-                    z++;
+                    iterations_counter++;
                     T temp = arr[j];
                     arr[j] = arr[j - step];
                     arr[j - step] = temp;
@@ -31,7 +22,6 @@ public class ShellSort<T extends Comparable<T>> {
                 }
             }
         }
-        iterations_counter += x;
         System.out.print(" Количество итераций: " + iterations_counter + ".");
     }
 }
